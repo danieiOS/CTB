6c534d8954047c88639ea988f6940431ac24543d
Seongwook Song <danielosong98@gmail.com>
2024-03-26 00:42:30 +0900

    [Bronze V] Title: 사칙연산, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let input = readLine()!
+let arr = input.split(separator: " ")
+let A = Int(arr[0])!
+let B = Int(arr[1])!
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A%B)
