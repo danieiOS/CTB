ee5097e93d1087341676c47dee3f9191a9771c3b
Seongwook Song <danielosong98@gmail.com>
2024-03-26 23:12:39 +0900

    [Bronze V] Title: 나머지, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let input = readLine()!
+let inputArr = input.split(separator: " ")
+let A = Int(inputArr[0])!
+let B = Int(inputArr[1])!
+let C = Int(inputArr[2])!
+
+print((A + B) % C)
+print(((A % C) + (B % C)) % C)
+print((A * B) % C)
+print(((A % C) * (B % C)) % C)
